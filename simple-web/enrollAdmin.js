79cc65a243958a9e46c227440338c5936c566eea
bstudent <saarc@ajou.ac.kr>
2021-11-26 11:38:33 +0900

    simple web add

---
+++ simple-web/enrollAdmin.js
@@ -0,0 +1,51 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+// SDK 에서 컴포넌트 사용
+const FabricCAServices = require('fabric-ca-client');
+const { FileSystemWallet, X509WalletMixin } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+
+// 연결정보 connection.json 파일 불러오기
+const ccpPath = path.resolve(__dirname,'connection.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+
+// JSON -> 구조체
+const ccp = JSON.parse(ccpJSON);
+
+async function main() {
+    try {
+
+        // Create a new CA client for interacting with the CA.
+        const caURL = ccp.certificateAuthorities['ca.org1.hydrogreen.com'].url;
+        const ca = new FabricCAServices(caURL);
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the admin user.
+        const adminExists = await wallet.exists('admin');
+        if (adminExists) {
+            console.log('An identity for the admin user "admin" already exists in the wallet');
+            return;
+        }
+
+        // Enroll the admin user, and import the new identity into the wallet.
+        const enrollment = await ca.enroll({ enrollmentID: 'admin', enrollmentSecret: 'adminpw' });
+        const identity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
+        wallet.import('admin', identity);
+        console.log('Successfully enrolled admin user "admin" and imported it into the wallet');
+
+    } catch (error) {
+        console.error(`Failed to enroll admin user "admin": ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
---
+++ simple-web/server-hydro.js
@@ -0,0 +1,129 @@
+// ExpressJS Setup
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+
+// Hyperledger Bridge
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+const ccpPath = path.resolve(__dirname,'connection.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// use static file
+app.use(express.static(path.join(__dirname, 'views')));
+
+// configure app to use body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// main page routing
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index-hydro.html');
+});
+
+
+// 사용자추가 라우팅
+app.post('/user', async(req, res)=>{
+    const customerid = req.body.customerid;
+ 
+    try {
+        console.log(`user post routing - ${customerid}`);
+
+        cc_call('adduser', [customerid], res)
+
+    }
+    catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+    }
+
+});
+
+// 충전이력생성 라우팅
+app.post('/charge', async(req, res)=>{
+    
+    //ncid, ncuid, ndate, namount, nprice, nplace
+    const ncid = req.body.ncid;
+    const ncuid = req.body.ncuid;
+    const ndate = req.body.ndate;
+    const namount = req.body.namount;
+    const nprice = req.body.nprice;
+    const nplace = req.body.nplace;
+
+    try {
+        console.log(`charge post routing - ${ncid}`);
+
+        cc_call('charge', [ncid,ncuid,namount,ndate,nplace,nprice], res)
+
+    }
+    catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+    }
+
+});
+
+// history
+app.get('/history', async(req, res)=>{
+
+    try {
+        const id = req.query.pid;
+        console.log(`history routing - ${id}`);
+
+        cc_call('history', [id], res)
+
+    }
+    catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+    }
+});
+
+async function cc_call(fn_name, args, res){
+
+
+    const walletPath = path.join(process.cwd(), 'wallet');
+    const wallet = new FileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    const userExists = await wallet.exists('user1');
+    if (!userExists) {
+        console.log(`cc_call`);
+        console.log('An identity for the user "user1" does not exist in the wallet');
+        console.log('Run the registerUser.js application before retrying');
+        return;
+    }
+    const gateway = new Gateway();
+    await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+    const network = await gateway.getNetwork('mygreen');
+    const contract = network.getContract('hydrogreen');
+
+    var result;
+
+    if(fn_name == 'adduser'){
+        result = await contract.submitTransaction('adduser', args[0]);
+        const myobj = {result: "success"}
+        res.status(200).json(myobj)
+    }else if(fn_name == 'charge'){
+        result = await contract.submitTransaction('charge', args[0],args[1],args[2],args[3],args[4],args[5]);
+        const myobj = {result: "success"}
+        res.status(200).json(myobj)
+    }else if(fn_name == 'history'){
+        result = await contract.evaluateTransaction('history', args[0]);
+        const myobj = JSON.parse(result)
+        res.status(200).json(myobj)
+    }else{
+        result = 'not supported function'
+    }
+
+    gateway.disconnect();
+
+    return ;
+}
+
+// server start
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
